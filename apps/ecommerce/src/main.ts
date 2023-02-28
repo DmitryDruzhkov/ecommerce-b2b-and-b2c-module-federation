b239d51d4f394803340bb35b866cacb859f5574b
Dmitry Druzhkov <dmitriy.druzhkov@gmail.com>
2023-02-28 22:40:34 +0300

    add shell

--- apps/ecommerce/src/main.ts
+++ apps/ecommerce/src/main.ts
@@ -1,11 +1,2 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import {
-  provideRouter,
-  withEnabledBlockingInitialNavigation,
-} from '@angular/router';
-import { AppComponent } from './app/app.component';
-import { appRoutes } from './app/app.routes';
-
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
-}).catch((err) => console.error(err));
+import('./bootstrap')
+	.catch(err => console.error(err));
