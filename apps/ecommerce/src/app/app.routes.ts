ebdf5162e6c20105cd49f85fab37aeb01bde4b34
Dmitry Druzhkov <dmitriy.druzhkov@gmail.com>
2023-03-16 22:21:43 +0300

    chenge route

--- apps/ecommerce/src/app/app.routes.ts
+++ apps/ecommerce/src/app/app.routes.ts
@@ -4,17 +4,16 @@ import { Route, RouterModule } from '@angular/router';
 
 export const appRoutes: Route[] = [
   { path: '', loadComponent: () => import('@ecommerce-b2b-and-b2c/auth/features').then(m => m.AuthComponent) },
-  /* { path: 'profile', loadChildren: () => import('@ecommerce-b2b-and-b2c/profile').then(m => m.AppModule) } */
-  /* {
+  /* { path: 'cart', loadChildren: () => import('cart/Module').then(m => m.AppModule) } */
+  {
     path: 'cart',
     loadChildren: () => loadRemoteModule({
         type: 'module',
         remoteEntry: 'http://localhost:4204/remoteEntry.js',
-        exposedModule: './Component'
+        exposedModule: './Module'
       })
       .then(m => m.AppModule)
-  } */
-  { path: 'cart', loadChildren: () => import('cart/Module').then(m => m.AppModule) }
+  }
 ];
 
 @NgModule({
